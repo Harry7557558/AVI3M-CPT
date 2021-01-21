77ac4879ada0d5e816b87a5c03e45729634eb9b0
Harry Chen <44143868+Harry7557558@users.noreply.github.com>
2021-01-21 10:35:49 -0500

    test

--- modeling/cup_model_1.cpp
+++ modeling/cup_model_1.cpp
@@ -11,6 +11,8 @@ std::vector<triangle> trigs;
 
 typedef std::function<vec3(double u, double v)> ParamSurface;
 
+
+
 void triangulate(ParamSurface F, int un, int vn) {
 	// u: 0-2pi; v: 0-1
 	for (int ui = 0; ui < un; ui++) {
@@ -34,6 +36,15 @@ void triangulate(ParamSurface F, int un, int vn) {
 		}
 	}
 }
+
+#include "triangulate/parametric_surface_adaptive_dist.h"
+void triangulate_adaptive(ParamSurface F, double tol, int un, int vn) {
+	std::vector<triangle_3d> T = AdaptiveParametricSurfaceTriangulator_dist(F).triangulate_adaptive(0., 2.*PI, 0., 1., un, vn, 16, tol, true, false);
+	for (int i = 0; i < (int)T.size(); i++)
+		trigs.push_back(triangle{ T[i][0], T[i][1], T[i][2], vec3(1.) });
+}
+
+
 
 double smoothstep(double x) {
 	//return x<0. ? 0. : x>1. ? 1. : x;
@@ -85,7 +96,11 @@ vec3 map_lower_1(double u, double v) {
 	return vec3(c.x*cossin(u), c.y);
 }
 vec3 map_lower(double u, double v) {
-	return smooth_fuse_G1(map_lower_0, map_lower_1, 0.2, 0.5, u, v);
+	vec2 c = mix(vec2(r0, 0), vec2(r0, r0_h2), v);
+	vec3 s1 = vec3(c.x*cossin(u), c.y);
+	vec3 s2 = smooth_fuse_G1(map_lower_0, map_lower_1, 0.2, 0.5, u, v);
+	return s2;
+	return mix(s2, s1, .6*(.5 - .5*tanh(200.*(.98 + cos(3.*u)))));
 }
 
 vec3 map_ring_0(double u, double v) {
@@ -112,6 +127,7 @@ vec3 map_bottom(double u, double v) {
 }
 
 vec3 map_cup(double u, double v) {
+	//return map_lower(u, v);
 	return smooth_fuse_G1(
 		map_bottom,
 		[](double u, double v) { return smooth_fuse_G1(map_lower, map_body, 0.1, 0.005, u, v); },
@@ -120,15 +136,34 @@ vec3 map_cup(double u, double v) {
 
 
 
+
 #define STB_IMAGE_IMPLEMENTATION
 #include ".libraries\stb_image.h"
 
+
 int main(int argc, char* argv[]) {
-	const int UN = 200;
-	const int VN = 200;
+
+#if 1
+#define triangulate triangulate_adaptive
+#endif
+
+	const int UN = 400;
+	const int VN = 400;
+	double tol = 1. / sqrt(UN*VN);
 
 	const double thickness = 0.01;
 	const double eps = 1e-5;
+
+	if (0) {
+		triangulate(map_cup,
+#ifdef triangulate
+			tol, 12, 12
+#else
+			UN, VN * 2
+#endif
+		);
+		goto Finish;
+	}
 
 	// outer layer
 	triangulate([&](double u, double v)->vec3 {
@@ -138,27 +173,35 @@ int main(int argc, char* argv[]) {
 		vec3 dpdv = (map_cup(u, v + eps) - map_cup(u, v - eps)) / (2.*eps);
 		vec3 n = ncross(dpdu, dpdv);
 		return p + thickness * n;
-	}, UN, VN * 2);
+	},
+#ifdef triangulate
+		tol, 12, 12
+#else
+		UN, VN * 2
+#endif
+		);
 
 	// color
-	struct rgba { uint8_t r, g, b; };
-	int W, H;
-	rgba *img = (rgba*)stbi_load("D:\\conch.jpg", &W, &H, nullptr, 3);
-	printf("%d %d\n", W, H);
-	vec3 *colors = new vec3[W*H];
-	for (int i = 0; i < W; i++) for (int j = 0; j < H; j++)
-		colors[(H - 1 - j)*W + i] = vec3(img[j*W + i].r, img[j*W + i].g, img[j*W + i].b) * (1. / 255.);
-	vec2 cupdim = vec2(2.*PI*r0, hypot(r1 - r0, h));
-	vec2 sc = 1. * vec2(W, H) * cupdim / min(cupdim.x, cupdim.y);
-	for (int t = 0; t < (int)trigs.size(); t++) {
-		vec3 p = (trigs[t].a + trigs[t].b + trigs[t].c) / 3.;
-		vec2 uv = vec2(atan2(p.y, p.x) / (2.*PI) + 0.5, p.z / h);
-		uv = (uv - vec2(0.5, 0.5))*sc + vec2(0.5*W, 0.5*H);
-		int i = (int)(uv.x), j = (int)(uv.y);
-		if (i >= 0 && i < W && j >= 0 && j < H) {
-			trigs[t].col = colors[j*W + i];
+	if (0) {
+		struct rgba { uint8_t r, g, b; };
+		int W, H;
+		rgba *img = (rgba*)stbi_load("D:\\nautilus.jpg", &W, &H, nullptr, 3);
+		printf("%d %d\n", W, H);
+		vec3 *colors = new vec3[W*H];
+		for (int i = 0; i < W; i++) for (int j = 0; j < H; j++)
+			colors[(H - 1 - j)*W + i] = vec3(img[j*W + i].r, img[j*W + i].g, img[j*W + i].b) * (1. / 255.);
+		vec2 cupdim = vec2(2.*PI*r0, hypot(r1 - r0, h));
+		vec2 sc = 1. * vec2(W, H) * cupdim / min(cupdim.x, cupdim.y);
+		for (int t = 0; t < (int)trigs.size(); t++) {
+			vec3 p = (trigs[t].a + trigs[t].b + trigs[t].c) / 3.;
+			vec2 uv = vec2(atan2(p.y, p.x) / (2.*PI) + 0.5, p.z / h);
+			uv = (uv - vec2(0.5, 0.5))*sc + vec2(0.5*W, 0.5*H);
+			int i = (int)(uv.x), j = (int)(uv.y);
+			if (i >= 0 && i < W && j >= 0 && j < H) {
+				trigs[t].col = colors[j*W + i];
+			}
+			//trigs[t].col *= vec3(0.9, 0.85, 0.65);
 		}
-		//trigs[t].col *= vec3(0.9, 0.85, 0.65);
 	}
 
 	// inner layer
@@ -170,7 +213,13 @@ int main(int argc, char* argv[]) {
 		vec3 dpdv = (map_cup(u, v + eps) - map_cup(u, v - eps)) / (2.*eps);
 		vec3 n = ncross(dpdu, dpdv);
 		return p - thickness * n;
-	}, UN, VN);
+	},
+#ifdef triangulate
+		tol, 12, 12
+#else
+		UN, VN * 2
+#endif
+		);
 
 	// mouth
 	triangulate([&](double u, double t)->vec3 {
@@ -184,9 +233,27 @@ int main(int argc, char* argv[]) {
 		vec3 d2 = -d1;
 		double t2 = t * t, t3 = t2 * t;
 		return (2.*t3 - 3.*t2 + 1.)*p1 + (t3 - 2.*t2 + t)*d1 + (-2.*t3 + 3.*t2)*p2 + (t3 - t2)*d2;
-	}, UN, VN / 20);
+	},
+#ifdef triangulate
+		tol, 12, 3
+#else
+		UN, VN / 20
+#endif
+		);
+
+	// place it on the plane
+	if (1) {
+		double min_z = INFINITY;
+		for (int i = 0; i < (int)trigs.size(); i++) {
+			min_z = min(min(min_z, trigs[i].a.z), min(trigs[i].b.z, trigs[i].c.z));
+		}
+		for (int i = 0; i < (int)trigs.size(); i++) {
+			trigs[i].a.z -= min_z, trigs[i].b.z -= min_z, trigs[i].c.z -= min_z;
+		}
+	}
 
 	// output
+Finish:;
 	std::vector<stl_triangle> stl;
 	for (int i = 0; i < (int)trigs.size(); i++) {
 		stl.push_back(stl_triangle(trigs[i].a, trigs[i].b, trigs[i].c, trigs[i].col));
