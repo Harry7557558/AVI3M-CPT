d33e87831d48f6524d73540c567629f2335c458e
Harry Chen <44143868+Harry7557558@users.noreply.github.com>
2021-01-28 18:42:24 -0500

    compress images and add missed files

---
+++ screenshots/png2jpg.py
@@ -0,0 +1,8 @@
+import os
+from PIL import Image
+
+files = [s for s in os.listdir() if s[-4:]=='.png']
+
+for file in files:
+    img = Image.open(file).convert('RGB')
+    img.save("min/"+file.replace('.png','.jpg'))
